be054435b7f4e655b80091b505af26601680fe8b
DkChriss <chrisshuayhua@hotmail.com>
2025-01-12 04:59:15 -0400

    Fix pagination for dtos

--- api/dev-test/src/main/java/com/vulcan/dev_test/DevTestApplication.java
+++ api/dev-test/src/main/java/com/vulcan/dev_test/DevTestApplication.java
@@ -2,8 +2,10 @@ package com.vulcan.dev_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class DevTestApplication {
 
 	public static void main(String[] args) {
